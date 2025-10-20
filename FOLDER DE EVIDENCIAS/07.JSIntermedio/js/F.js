b99a1aebb6f620f4523f8115059e509f570b43aa
EnriqueTablerosGarcia <etablerosg2300@alumno.ipn.mx>
2025-10-20 02:53:10 -0600

    al raton quedan el 3 y 4, pero ocupo dormir profre, gracias :D

--- FOLDER DE EVIDENCIAS/07.JSIntermedio/js/F.js
+++ FOLDER DE EVIDENCIAS/07.JSIntermedio/js/F.js
@@ -22,3 +22,37 @@ function problema1() {
 
   document.getElementById("p1-output").textContent = "Resultado: " + resultado;
 }
+
+//  PROBLEMA 2
+function problema2() {
+  var n1 = document.getElementById("p2-num1").value;
+  var n2 = document.getElementById("p2-num2").value;
+  var n3 = document.getElementById("p2-num3").value;
+
+  if (n1 === "" || n2 === "" || n3 === "") {
+    document.getElementById("p2-output").textContent = "Por favor ingresa los tres números.";
+    return;
+  }
+
+  n1 = Number(n1);
+  n2 = Number(n2);
+  n3 = Number(n3);
+
+
+  if (n1 === n2 || n1 === n3 || n2 === n3) {
+    document.getElementById("p2-output").textContent = "Los 3 números deben ser diferentes";
+    return;
+  }
+
+  var mayor;
+
+  if (n1 > n2 && n1 > n3) {
+    mayor = n1;
+  } else if (n2 > n1 && n2 > n3) {
+    mayor = n2;
+  } else {
+    mayor = n3;
+  }
+
+  document.getElementById("p2-output").textContent = "El número mayor es: " + mayor;
+}
