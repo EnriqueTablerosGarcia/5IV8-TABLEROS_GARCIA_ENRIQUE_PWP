1673875cf000436c87926d471ac3aeb51d0f1b2f
EnriqueTablerosGarcia <etablerosg2300@alumno.ipn.mx>
2025-10-20 23:36:32 -0600

    JUEGO CASI COMPLETO

--- FOLDER DE EVIDENCIAS/08.Rompecabezas/js/juego.js
+++ FOLDER DE EVIDENCIAS/08.Rompecabezas/js/juego.js
@@ -1,127 +1,242 @@
-var instrucciones =[
-
-    "Utiliza las flechas de navegacion para mover las piezas",
-    "Para Ordenar las piezas guiate por la imagen Objetivo"
-    
+var instrucciones = [
+    "Utiliza las flechas de navegación para mover las piezas jeje",
+    "Para ordenar las piezas guiate por la imagen objetivo"
 ];
 
-//vamos a guardar dentro de una variable, los movs del rompe
-var movimientos = [
+//vamos a guardar dentro de una variable los movimeintos del rompecabezas
+var movimientos = [];
 
-
-];
-
-//matriz para saber las posiciones del rompecab
-//sin ( ) es una matriz lineal
+//vamos a crear una matriz para saber las posiciones del rompecabezas
 var rompe = [
-(1,2,3),
-(4,5,6),
-(7,8,9)
-
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
 ];
 
-//vamos a tener que crear una matriz donde tengamos las pos. correctas
-var rompecorrecta = [
-(1,2,3),
-(4,5,6),
-(7,8,9)
+//vamos a tener que crear una matriz donde tengamos las posiciones correctas
 
+var rompeCorrecta = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
 ];
 
-
-// necesito saber las corrds d la pza vacia. la q se va a mover 
-
-var filavVacia = 2 ;
+//necesito saber las coordenadas de la pieza vacia, la que se va a mover
+var filaVacia = 2;
 var columnaVacia = 2;
 
-function mostrarInstrucciones (instrucciones) {
-for(var i=0; i<instrucciones.length; i++){
-mostrarInstruccionesLista(instrucciones[i],"lista-instrucciones")
+//necesitamos ahora si una funcion que se encargue de mostrar las instrucciones
 
-
-}
-
-
-}
-//esta func se encarga de crear el comp li y agregar la lista d dichas instr.
-
-
-function mostrarInstruccionesLista (instruccion, idlista){
-var ul = document.getElementById(idlista);
-var li =document.createElement("li");
-li.textContent = instruccion;
-ul.appendChild(li);
-
-}
-
-//func para saber si esta bien papas con chile
-
-function checarsigano(){
-for (var i = 0; i<rompe.length; i++){
-for(var j = 0; j<rompe[i].length;j++){
-    var rompeActual = rompe [i][j];
-    if (rompeActual !== rompecorrecta=[i][j]){
-return false;
+function mostrarInstrucciones(instrucciones){
+    for(var i = 0; i < instrucciones.length; i++){
+        mostrarInstruccionesLista(instrucciones[i], "lista-instrucciones");
     }
 }
 
+//esta funcion se encarga de crear el componente li y agregar la lista de dichas instrucciones
 
+function mostrarInstruccionesLista(instruccion, idLista){
+    var ul = document.getElementById(idLista);
+    var li = document.createElement("li");
+    li.textContent = instruccion;
+    ul.appendChild(li);
 }
-return true;
 
+//vamos a crear una funcion para saber que gano
+function checarSiGano(){
+    for(var i = 0; i < rompe.length; i++){
+        for(var j = 0; j < rompe[i].length; j++){
+            var rompeActual = rompe[i][j];
+            if(rompeActual !== rompeCorrecta[i][j]){
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
-
-//mostrar en html q se gano
+//mostrar en html si se gano
 function mostrarCartelGanador(){
-
-if (checarsigano()){
-alert("Felicidades, ganaste el juego carnal")
-     
-}
-return false
-
+    if(checarSiGano()){
+        alert("¡Felicidades, ganaste jeje!");
+    }
+    return false
 }
 
 /*
-necesitamos una func, q se encargue de poder intercambiar las posiciones d la pza vacias vs cualquiera, para esto, tenemos q hacer uso de:
-arreglo[][] = posicion [][]
-//intercambiar
-posicion [][] = arreglo [][]
-posicion [][] = arreglo [ ] []
-
-
-
+    necesitamos una funcion que se encargue de poder intercambiar las posiciones de la pieza vacia vs la de cualquiera, patra esto tenemos que hacer uso de:
+    arreglo[][] = posicion[][]
+    //intercambiar
+    posicion[][] = arreglo[][]
 */
 
+function intercambiarPosicionesRompe(filaPos1, columnaPos1, filaPos2, columnaPos2){
+    var pos1 = rompe[filaPos1,columnaPos1];
+    var pos2 = rompe[filaPos2, columnaPos2];
 
-function intercambiarposicionesrompe(filapos1,columnapos1, filapos2,columnapos2){
+    //intercambio
 
-    var pos1 = rompe[filapos1,columnapos1],
-        var pos2 = rompe[filapos2,columnapos2],
+    rompe[filaPos1, columnaPos1] = pos2;
+    rompe[filaPos2, columnaPos2] = pos1;
+}    
 
-        //inter
-        rompe[filapos1, columnapos1] = pos2;
-        rompe[filapos2, columnapos2] = pos1 ;
+//crear una funcion que se encargue de saber donde esta la pieza vacia
+function actualizarPosicionVacia(nuevaFila, nuevaColumna){
+    filaVacia = nuevaFila;
+    columnaVacia = nuevaColumna;
 }
+
+//necesitamos tmb limitar las posiciones del rompecabezas
+function posicionValida(fila, columna){
+    return (fila >= 0 && fila <= 2 && columna >= 0 && columna <= 2);
+}
+
+//debemos crear una funcion que se encargue del movimiento detectando el evento de las flechas de navegacion
+//debemos crear una matriz de identificacion de movimiento
+// arriba 38, abajo 40, izquierda 37, derecha 39
+
+
+
+var codigosDireccion = {
+    /CLAVE/IZQUIERDA : 37/VALOR/ ,
+    ARRIBA : 38,
+    DERECHA : 39,
+    ABAJO : 40
+};//Este es formato JSON, se parece a un diccionario en python, esta es una lista
+
+function moverEnDireccion(direccion){
+    var nuevaFilaPiezaVacia;
+    var nuevaColumnaPiezaVacia;
+
+    //si se mueve
+
+    if(direccion === codigosDireccion.ABAJO){
+        nuevaFilaPiezaVacia = filaVacia + 1;
+        nuevaColumnaPiezaVacia = columnaVacia;
+    }
+    else if(direccion === codigosDireccion.ARRIBA){
+        nuevaFilaPiezaVacia = filaVacia - 1;
+        nuevaColumnaPiezaVacia = columnaVacia;
+    }
+    else if(direccion === codigosDireccion.DERECHA){
+        nuevaFilaPiezaVacia = filaVacia;
+        nuevaColumnaPiezaVacia = columnaVacia + 1;
+    }
+    else if(direccion === codigosDireccion.IZQUIERDA){
+        nuevaFilaPiezaVacia = filaVacia;
+        nuevaColumnaPiezaVacia = columnaVacia - 1;
+    }
+
+    //solo mando a llamar a que la posicion sea valida
+    if(posicionValida(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia)){
+        //tengo q hacer una funcion q se encargue de intercambiar las posiciones
+        intercambiarPosiciones(filaVacia, columnaVacia, nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+        actualizarPosicionVacia(nuevaFilaPiezaVacia, nuevaColumnaPiezaVacia);
+        //tengo que guardar el ultimo movimiento porque lo tewngo que mostrar
+
+        agregarUltimoMovimiento(direccion);
+    }
+}
+
+function intercambiarPosiciones(fila1, columna1, fila2, columna2){
+    var pieza1 = rompe[fila1,columna1];
+    var pieza2 = rompe[fila2, columna2];
+
+    //intercambio ya debe de ser por parte de los frames y el html
+
+    intercambiarPosicionesRompe(fila1, columna1, fila2, columna2);
+    //para el html
+    intercambiarPosicionesDOM('pieza'+ pieza1, 'pieza'+ pieza2);
+} 
+
+function intercambiarPosicionesDOM(idPieza1, idPieza2){
+    var pieza1 = document.getElementById(idPieza1);
+    var pieza2 = document.getElementById(idPieza2);
+
+    //vamos a clonarlas
+    var padre = elementoPieza1.parentNode;
+
+    //lo clono
+
+    var clonElemento1 = elementoPieza1.cloneNode(true);
+    var clonElemento2 = elementoPieza2.cloneNode(true);
+
+    //reemplazar a los padres con sus clones
+
+    padre.replaceChild(clonElemento1, elementoPieza2);
+    padre.replaceChild(clonElemento2, elementoPieza1);
+}
+
+//debo de actualizar los movs en el DOM tmb
+function actualizarUltimoMovimeinto(direccion){
+    var ultimoMovimiento = document.getElementById("flecha");
+    switch(direccion){
+        case codigosDireccion.ARRIBA:
+            ultimoMovimiento.textContent = "↑";
+            break;
+        case codigosDireccion.ABAJO:
+            ultimoMovimiento.textContent = "↓";
+            break;
+        case codigosDireccion.IZQUIERDA:
+            ultimoMovimiento.textContent = "←";
+            break;
+        case codigosDireccion.DERECHA:
+            ultimoMovimiento.textContent = "→";
+            break;
+    }
+}
+
+//necesitamos poder mezclar todas las piezas
+
+function mezclarPiezas(){
+    if(veces <= 0){
+        alert("Asi no se puede");
+        return;
+    }
+
+    var direcciones = [codigosDireccion.ABAJO, codigosDireccion.ARRIBA, codigosDireccion.DERECHA, codigosDireccion.IZQUIERDA];
+
+    var direccion = direcciones[Math.floor(Math.random() * direcciones.length)];
+
+    moverEnDireccion(direccion);
+
+    setTimeout(function(){
+        mezclarPiezas(veces - 1);
+    }, 100);
+}
+
+//necesitamos saber que teclas se estan oprimiendo
+
+function capturarTeclas(){
+    document.body.onkeydown = (function(evento){
+        if(evento.which === codigosDireccion.ARRIBA ||
+                         evento.which === codigosDireccion.ABAJO ||
+                         evento.which === codigosDireccion.DERECHA ||
+                         evento.which === codigosDireccion.IZQUIERDA){
+            moverEnDireccion(evento.which);
+            //saber si gane
+            var gano = checarSiGano();
+            if(gano){
+                setTimeout(function(){
+                    mostrarCartelGanador();
+                }, 500);
+            }
+            evento.preventDefault();
+        }
+
+    });
+}
+
 
 
 function iniciar(){
-//mezclar piezukis
-//si cap el mov
-
-
-
+    //mezclar las piezas
+    //capturar el ultimo movimiento}
+    mezclarPiezas(30);
+    capturarTeclas();
+   
 }
 
-//mandamos traer la func
+//mandamos traer a la funcion
 
-mostrarInstrucciones(instrucciones);
-
-
-
-
-
-
-
-
+mostrarInstrucciones(instrucciones); 
